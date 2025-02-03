bbc20a54312ab7980298abb220beb748e2dd3e44
Anders Høgden <andeshog@stud.ntnu.no>
2025-02-03 14:18:50 +0100

    formatting

--- joy_to_pwm/include/joy_to_pwm/gripper_interface.hpp
+++ joy_to_pwm/include/joy_to_pwm/gripper_interface.hpp
@@ -1,24 +1,23 @@
 #ifndef GRIPPER_INTERFACE_HPP
 #define GRIPPER_INTERFACE_HPP
 
-#include "joy_to_pwm/gripper_interface_driver.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joy.hpp>
+#include "joy_to_pwm/gripper_interface_driver.hpp"
 
-class GripperInterface : public rclcpp::Node
-{
-    public:
-        GripperInterface();
+class GripperInterface : public rclcpp::Node {
+   public:
+    GripperInterface();
 
-    private:
-        void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
+   private:
+    void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
 
-        short i2c_bus_;
-        short i2c_address_;
+    short i2c_bus_;
+    short i2c_address_;
 
-        std::unique_ptr<GripperInterfaceDriver> gripper_driver_;
+    std::unique_ptr<GripperInterfaceDriver> gripper_driver_;
 
-        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
 };
 
-#endif // GRIPPER_INTERFACE_HPP
+#endif  // GRIPPER_INTERFACE_HPP
