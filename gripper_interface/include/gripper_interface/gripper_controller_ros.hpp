b906d7475a354a60ea1cec3e0a2457b089696f47
Patrick Sheehan <patricsh@stud.ntnu.no>
2026-01-31 22:04:16 +0100

    initial: initial commit

---
+++ gripper_interface/include/gripper_interface/gripper_controller_ros.hpp
@@ -0,0 +1,8 @@
+#ifndef GRIPPER_CONTROLLER_DP__GRIPPER_CONTROLLER_ROS_HPP_
+#define GRIPPER_CONTROLLER_DP__GRIPPER_CONTROLLER_ROS_HPP_
+
+
+class GripperControllerNode()  : public rclcpp::Node {
+  public: 
+    GripperControllerNode(); 
+}; 
