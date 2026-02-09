af50e2c288623867e7c358fce4a212eb2a7a322a
Patrick Sheehan <patricsh@stud.ntnu.no>
2026-02-09 20:42:34 +0100

    moved controller module related files originally made in interface folder as well as removed the previously added dependencies as consequence of making the controller module within the interface

--- gripper_interface/include/gripper_interface/gripper_controller_ros.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GRIPPER_CONTROLLER_DP__GRIPPER_CONTROLLER_ROS_HPP_
-#define GRIPPER_CONTROLLER_DP__GRIPPER_CONTROLLER_ROS_HPP_
-
-
-class GripperControllerNode()  : public rclcpp::Node {
-  public: 
-    GripperControllerNode(); 
-}; 
