3186fd37b975542ec7863aa694648b63322eb89d
forrisdahl <nathanif@stud.ntnu.no>
2026-04-19 17:01:21 +0200

    fix: init can

--- gripper_interface/src/gripper_interface_node.cpp
+++ gripper_interface/src/gripper_interface_node.cpp
@@ -13,6 +13,8 @@ GripperInterface::GripperInterface() : Node("gripper_interface_node") {
     gripper_driver_ =
         std::make_unique<GripperInterfaceDriver>(pwm_gain_, pwm_idle_);
 
+    gripper_driver_.init_can();
+
     watchdog_timer_ = this->create_wall_timer(
         std::chrono::milliseconds(500),
         std::bind(&GripperInterface::encoder_angles_callback, this));
